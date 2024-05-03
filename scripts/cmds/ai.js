aabc4b7c0b05d35c5eb905cd66ab87cbcb4f62f7
MeganAe <168326500+MeganAe@users.noreply.github.com>
2024-05-03 22:39:09 +0200

    Update ai.js

--- scripts/cmds/ai.js
+++ scripts/cmds/ai.js
@@ -33,7 +33,7 @@ module.exports = {
       }
 
 
-      const response = await axios.get(`https://aryanapis.replit.app/gpt?prompt=hello${encodeURIComponent(prompt)}`);
+      const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=hello${encodeURIComponent(prompt)}`);
       const answer = response.data.answer;
 
  
